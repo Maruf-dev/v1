9621b71c9620b1694d3f2fcd9d76719002e4744c
maruf-dev <maruf123erkinov@gmail.com>
2024-08-29 21:26:40 +0500

    add img

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,32 +135,20 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Maruf and I enjoy creating things that live on the internet. My
+              interest in web development started back in 2019 when I decided to try learning Python
+              — turns out playing with Turtle module taught me a lot about Python!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://www.teamit.uz//">Teamit academy</a>,{' '}
+              <a href="https://kitobland.uz/">a start-up</a>,{' '}
+              <a href="https://jizzakhps.uz/">a Presidential School</a>, and also I had been working
+              as a Project manager at sevelar projects. My main focus these days is building
+              accessible, inclusive products and digital experiences at{' '}
+              <a href="https://codevision.uz/">Codevision</a> for a variety of clients.
             </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
 
